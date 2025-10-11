3b706f96e788c070fb8d40794caca5985920c1b0
CRISTIAN PIEREZAN SAMORO <03985535051@senacrs.edu.br>
2025-10-11 11:53:20 -0300

    aula 11/10

--- src/main/java/com/example/vitrine/model/Produto.java
+++ src/main/java/com/example/vitrine/model/Produto.java
@@ -1,5 +1,62 @@
 package com.example.vitrine.model;
 
+import java.math.BigDecimal;
+
+
 public class Produto {
+    private Long id;
+    private String name;
+    private String descricao;
+    private BigDecimal preco;
+    private String urlImagem;
     
+    public Produto(Long id, String name, String descricao, BigDecimal preco, String urlImagem){
+        this.id = id;
+        this.name = name;
+        this.descricao = descricao;
+        this.preco = preco;
+        this.urlImagem = urlImagem;
+    }
+
+    public Long getId(){
+        return id;
+    }
+
+    public void setId(){
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
+    }
+
+    public String getUrlImagem() {
+        return urlImagem;
+    }
+
+    public void setUrlImagem(String urlImagem) {
+        this.urlImagem = urlImagem;
+    }
+
+
 }
--- src/main/java/com/example/vitrine/service/CatalogoService.java
+++ src/main/java/com/example/vitrine/service/CatalogoService.java
@@ -1,5 +1,18 @@
 package com.example.vitrine.service;
+import java.util.List;
+import java.util.ArrayList;
 
+import org.springframework.stereotype.Service;
+
+import com.example.vitrine.model.Produto;
+
+@Service
 public class CatalogoService {
-    
+    private List<Produto> produtos = new ArrayList<>();
+    private long proximoId = 1L;
+    public CatalogoService(){}
+
+    public void salvar(Produto produto){
+        
+    }
 }
